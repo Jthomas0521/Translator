fbeb131ee167c744d9c3d6cd59d13e0e0d4f9677
jahquan.thomas <jahquan.thomas@caci.com>
2023-11-09 16:01:36 -0500

    sample.docx

--- src/docfile.py
+++ src/docfile.py
@@ -1,1 +1,6 @@
 import docx
+
+doc = docx.Document('sample.docx')
+paras = [p.text for p in doc.paragraphs if p.text]
+
+print(f'=== Output type is a {type(paras)} of {type(paras[1])} \n total length is {len(paras)} ===')
